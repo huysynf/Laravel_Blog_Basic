e18e291d7e037eb00c704b90e94fe118563b8c0d
Huyhq <huyhq@deha-soft.com>
2022-04-22 23:57:12 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -127,7 +127,7 @@
                             type="password" name="password" required
                             autocomplete="current-password"
                             autofocus
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5 px-5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@gmail.com">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5 px-5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
                 </div>
             </div>
 
