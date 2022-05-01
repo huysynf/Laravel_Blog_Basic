adcdf020d8dcc64fd5694826c07200e9eb73cb8f
Huyhq <huyhq@deha-soft.com>
2022-05-01 20:28:37 +0700

    update provider

---
+++ app/Helpers/RouteHelper.php
@@ -0,0 +1,27 @@
+<?php
+if (! function_exists('includeRoutes')) {
+    /**
+     * Loops through a folder and requires all PHP files
+     * Searches sub-directories as well.
+     *
+     * @param $folder
+     */
+    function includeRoutes($folder)
+    {
+        try {
+            $rdi = new RecursiveDirectoryIterator($folder);
+            $it = new RecursiveIteratorIterator($rdi);
+
+            while ($it->valid()) {
+                if (! $it->isDot() && $it->isFile() && $it->isReadable() && $it->current()->getExtension() === 'php') {
+                    require $it->key();
+                }
+
+                $it->next();
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+
