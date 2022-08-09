31123bfb05b45575d154d780581a442b43b51793
Hạ Quang Huy <haqhuy1999@gmail.com>
2022-08-09 03:41:00 +0000

    Update app/Repositories/Users/UserRepository.php

--- app/Repositories/Users/UserRepository.php
+++ app/Repositories/Users/UserRepository.php
@@ -19,4 +19,54 @@ class UserRepository extends BaseRepository
     {
         return $this->model->withEmail($email)->first();
     }
+
+    /**
+     * Save a new entity in repository
+     * @param array $input
+     * @return
+     */
+    public function create(array $input)
+    {
+        return $this->model->create($input);
+    }
+
+    /**
+     * Update a entity in repository by id
+     * @param array $input
+     * @param $id
+     * @return BaseRepository
+     */
+    public function update(array $input, $id): self
+    {
+        $model = $this->model->findOrFail($id);
+        $model->fill($input);
+        $model->save();
+
+        return $model;
+    }
+
+    /**
+     * Delete a entity in repository by id
+     *
+     * @param $id
+     *
+     * @return int
+     */
+    public function destroy($id): int
+    {
+        return $this->model->destroy($id);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id): mixed
+    {
+        $model = $this->model->findOrFail($id);
+        $model?->delete();
+
+        return $model;
+    }
+
 }
